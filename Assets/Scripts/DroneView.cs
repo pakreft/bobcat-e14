9d1d7b29672104f54d5294c40fd842f9fe974a3c
pakreft <113921665+pakreft@users.noreply.github.com>
2024-02-01 17:12:48 +0100

    Setup NavMesh

--- Assets/Scripts/DroneView.cs
+++ Assets/Scripts/DroneView.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UI;
 
 public class DroneView : MonoBehaviour
 {
     [SerializeField] private Camera m_droneCamera;
-    [SerializeField] private Transform m_spot;
+    [SerializeField] private Transform m_destination;
+    [SerializeField] private NavMeshAgent m_agent;
 
     private RectTransform m_rectTransform;
     private RawImage m_rawImage;
@@ -57,7 +59,10 @@ public class DroneView : MonoBehaviour
                 Vector3 hitPoint = ray.GetPoint(enterDistance);
                 
                 // Step 4: Set position of the yellow spot
-                m_spot.position = new Vector3(hitPoint.x, m_spot.position.y, hitPoint.z);
+                m_destination.position = new Vector3(hitPoint.x, m_destination.position.y, hitPoint.z);
+
+                // Move the NavMeshAgent
+                m_agent.destination = m_destination.position;
             }
         }
     }
